85a13ecf2c8190911995ab62f6c57a7910f1c078
s-kanabe <s.kanabe.oct@gmail.com>
2019-10-28 18:58:19 +0900

    メッソッド内にセッターを移動

--- src/dao/DrinkDAO.java
+++ src/dao/DrinkDAO.java
@@ -1,29 +1,31 @@
 package dao;  
 
-import model.DrinkEntity;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
+import model.DrinkEntity;
 public class DrinkDAO {
-	//各レコードとなるDrinkEntityのインスタンスを生成 
-	DrinkEntity beer = new DrinkEntity();
-	DrinkEntity wine = new DrinkEntity();
-	
-	//セッターでビールレコード・ワインレコードの値を設定
-	beer.setId(1);
-	beer.setDrinkName("ビール");
-	beer.setAlcoholRate(0.03);
-	
-	wine.setId(2);
-	wine.setDrinkName("ワイン");
-	wine.setAlcoholrate(0.15);
-	
-	//Listに各レコードを格納
-	List<DrinkEntity> drinkList = new ArrayList<DrinkEntity>();
-	drinkList.add(beer);
-	drinkList.add(wine);
+
 	
 	public List<DrinkEntity> findAll() {
+		//各レコードとなるDrinkEntityのインスタンスを生成 
+		DrinkEntity beer = new DrinkEntity();
+		DrinkEntity wine = new DrinkEntity();
+		
+		//セッターでビールレコード・ワインレコードの値を設定
+		beer.setId(1);
+		beer.setDrinkName("ビール");
+		beer.setAlcoholRate(0.03);
+		
+		wine.setId(2);
+		wine.setDrinkName("ワイン");
+		wine.setAlcoholRate(0.15);
+		
+		//Listに各レコードを格納
+		List<DrinkEntity> drinkList = new ArrayList<DrinkEntity>();
+		drinkList.add(beer);
+		drinkList.add(wine);
+		
 		return drinkList;
 	}
 	
--- src/dao/GlassDAO.java
+++ src/dao/GlassDAO.java
@@ -1,29 +1,31 @@
 package dao;
 
-import model.GlassEntity;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+
+import model.GlassEntity;
 
 public class GlassDAO {	
-	//各レコードとなるGlassEntityのインスタンスを生成
-	GlassEntity middleGlass = new GlassEntity();
-	GlassEntity wineGlass = new GlassEntity();
-	
-	//セッターで中ジョッキレコード・ワイングラスレコードの値を設定 
-	middleGlass.setId(1);  
-	middleGlass.setGlassname("中ジョッキ");
-	middleGlass.setAmount(300);
-
-	wineGlass.setId(2);  
-	wineGlass.setGlassname("ワイングラス");
-	wineGlass.setAmount(100);
-
-	//Listに各レコードを格納
-	List<GlassEntity> glassList = new ArrayList<GlassEntity>();
-	glassList.add(middleGlass);
-	glassList.add(wineGlass);
 	
 	public List<GlassEntity> findAll() {
+		//各レコードとなるGlassEntityのインスタンスを生成
+		GlassEntity middleGlass = new GlassEntity();
+		GlassEntity wineGlass = new GlassEntity();
+		
+		//セッターで中ジョッキレコード・ワイングラスレコードの値を設定 
+		middleGlass.setId(1);  
+		middleGlass.setGlassName("中ジョッキ");
+		middleGlass.setAmount(300);
+
+		wineGlass.setId(2);  
+		wineGlass.setGlassName("ワイングラス");
+		wineGlass.setAmount(100);
+
+		//Listに各レコードを格納
+		List<GlassEntity> glassList = new ArrayList<GlassEntity>();
+		glassList.add(middleGlass);
+		glassList.add(wineGlass);
+		
 		return glassList;
 	}
 		
