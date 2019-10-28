41763cdc57d20c188c4413b79c598783913bc746
s-kanabe <s.kanabe.oct@gmail.com>
2019-10-28 19:19:24 +0900

    下記２点実装 DAOのメソッドを実行してListにセットする フォワード

--- src/servlet/MakeRecordServlet.java
+++ src/servlet/MakeRecordServlet.java
@@ -1,13 +1,20 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import dao.DrinkDAO;
+import dao.GlassDAO;
+import model.DrinkEntity;
+import model.GlassEntity;
 
 @WebServlet("/MakeRecordServlet")
 public class MakeRecordServlet extends HttpServlet {
@@ -16,13 +23,19 @@ public class MakeRecordServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		
-		//アプリケーションスコープに酒と量のListが入っていないかをチェックし、（nullかどうか）入っていればスルー
+		//アプリケーションスコープに酒と量のListが入っていないかをチェックし、入っていればスルー
 		if( !=null) {
 			
 		}
 		
 		//入っていなければ、酒と量のDAOのfindAllメソッドをそれぞれ実行して、戻ったListをセットする。
+		DrinkDAO drinkDAO = new DrinkDAO();
+		GlassDAO glassDAO = new GlassDAO();
 		
+		List<DrinkEntity> drinkList = drinkDAO.findAll();
+		List<GlassEntity> glassList = glassDAO.findAll();
+		
+		//アプリケーションスコープにレコードを保存
 		
 		
 		//計算ページのjspにフォワードさせる。
